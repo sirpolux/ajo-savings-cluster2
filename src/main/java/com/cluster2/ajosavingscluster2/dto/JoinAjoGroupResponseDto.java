f76c5ac71d233cb63c948ce8ff78b660b9a0f77f
Oluwatomiwo <danieloluwatomiwo@gmail.com>
2024-02-23 21:07:40 +0100

    Added endpoint for User to join_ajo_group

---
+++ src/main/java/com/cluster2/ajosavingscluster2/dto/JoinAjoGroupResponseDto.java
@@ -0,0 +1,14 @@
+package com.cluster2.ajosavingscluster2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class JoinAjoGroupResponseDto {
+    private String message;
+}
--- src/main/java/com/cluster2/ajosavingscluster2/service/AjoGroupService.java
+++ src/main/java/com/cluster2/ajosavingscluster2/service/AjoGroupService.java
@@ -2,8 +2,12 @@ package com.cluster2.ajosavingscluster2.service;
 
 import com.cluster2.ajosavingscluster2.dto.AjoGroupDto;
 import com.cluster2.ajosavingscluster2.dto.AjoGroupResponse;
+import com.cluster2.ajosavingscluster2.dto.JoinAjoGroupRequestDto;
+import com.cluster2.ajosavingscluster2.dto.JoinAjoGroupResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface AjoGroupService{
     ResponseEntity<AjoGroupResponse> createAjoGroup(AjoGroupDto ajoGroupRequest);
+
+    ResponseEntity<JoinAjoGroupResponseDto> joinGroup(JoinAjoGroupRequestDto joinGroupRequestDto);
 }
