921e1a5f5ea203061fe8ef5c2fdd2134b069ed99
Oluwatomiwo <danieloluwatomiwo@gmail.com>
2024-03-25 20:58:32 +0100

    Endpoint for transferring funds from global wallet to personal wallet

--- src/main/java/com/cluster2/ajosavingscluster2/repository/PersonalWalletRepository.java
+++ src/main/java/com/cluster2/ajosavingscluster2/repository/PersonalWalletRepository.java
@@ -4,4 +4,5 @@ import com.cluster2.ajosavingscluster2.model.PersonalWallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonalWalletRepository extends JpaRepository<PersonalWallet,Long> {
+    PersonalWallet findByUserId(java.lang.Long userId);
 }
